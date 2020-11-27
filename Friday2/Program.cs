b56733fb90f6f9fde9984547a6847d75c8cd49a2
Opilane TTHK <opilane@tthk.ee>
2020-11-27 10:01:59 +0200

    Not enought time

---
+++ Friday2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Friday2
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+                Console.WriteLine($"The cat {name} has been created!");
+            }
+
+            public void Sleep()
+            {
+                Console.WriteLine("Cat Sleeps");
+                hunger += 0.2;
+
+            }
+
+            public void Meow()
+            {
+                Console.WriteLine("Muda Muda Muda");
+            }
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("Spots", "Orange");
+            Console.WriteLine($"The level of happiness: {newCat.Hunger}");
+            
+            
+            while (newCat.Hunger != 1)
+            {
+                newCat.Sleep();
+                
+            }
+            newCat.Meow();
+            Console.WriteLine($"The level of happiness: {newCat.Hunger}");
+
+        }
+    }
+}
---
+++ Pet/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Pet
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            double age;
+            double weight;
+            double lifespan;
+
+            public Pet(string _name)
+            {
+                name = _name;
+                weight = 0;
+                age = 0;
+
+            }
+
+            public void PetInfo()
+            {
+                Console.WriteLine($"Name: {name}; Age: {age}; Weight: {weight}; Lifespan: {lifespan} ");
+            }
+
+            public double Lifespan
+            {
+                get { return lifespan; }
+            }
+
+            public double Weight
+            {
+                get { return weight; }
+            }
+
+            public double Age
+            {
+                get { return age; }
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Pet newPet = new Pet("hamster");
+
+
+        }
+    }
+}
